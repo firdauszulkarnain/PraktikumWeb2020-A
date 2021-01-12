8d4963a6f3384635225e9a3aa201ef5dc04cc3e4
firdauszulkarnain <muhammadfirdauszulkarnain03@gmail.com>
2021-01-12 10:44:28 +0800

    Tugas Akhir

---
+++ UAS-KELOMPOK5/findDecor/app/views/about/index.php
@@ -0,0 +1,57 @@
+<!-- Caraousel Start -->
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img class="d-block w-100" src="<?= URL_IMG ?>caraousel/caraousel4.png" alt="First slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="<?= URL_IMG ?>caraousel/caraousel5.png" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="<?= URL_IMG ?>caraousel/caraousel1.png" alt="Third slide">
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+
+<p class="sub-tittle text-uppercase font-weight-bold text-center mt-4">Tentang Kami</p>
+<hr class="mb-5 mt-n2" width="20%">
+
+<div class="container deskripsi mb-5">
+    <h1>
+        <i class="fas fa-minus"></i>
+        DESKRIPSI
+    </h1>
+    <p class="text-justify">Halo sahabat yang luar biasa, pada kesempatan kali ini izinkan kami dari findDécor untuk memperkenalkan diri. Mudah-mudahan dengan adanya perkenalan ini sahabat-sahabat sekalian akan lebih senang dengantoko online ini.
+
+        Sungguh terhormat bagi kami, jika Anda datang ke toko ini dan bisa memperoleh banyak hal yang bermanfaat.
+
+        findDécor adalah sebuah toko yang menyediakan jasa peminjaman dekorasi pernikahan. Bermula dari sebuah bangunan yang berlokasi di Kampus Ilkom, toko kami akhirnya bisa menjangkau pasar yang lebih luas lagi melalui internet.
+
+        Seiring dengan semakin berkembangnya teknologi, maka kami mencoba berinovasi dalam berbisnis. Salah satu bentuk inovasinya adalah dengan menyediakan berbagai layanan toko kami melalui media internet, dan toko online ini adalah salah satu bentuk pelayanan kami kepada Anda.
+
+        findDécor didirikan oleh Kelompok 5 pada 18 Desember 2020. Keberadaan dari toko online ini diharapkan dapat mempermudah Anda untuk mendapatkan berbagai produk berkualitas tinggi namun dengan harga yang terjangkau.
+
+        Dalam memberi layanan, kami selalu mencoba memberi persembahan terbaik kepada siapapun. Selain itu kami juga selalu menjunjung tinggi nilai-nilai etika yang baik, seperti kejujuran, ketepatan, dan profesionalitas dalam berbisnis. Mudah-mudahan dengannya toko online kami bisa memberi banyak manfaat bagi Anda.
+
+        Sekian dulu perkenalan ini. Semoga perkenalan ini bisa memberi inspirasi dan manfaat bagi Anda.
+
+        Salam super dan semoga Anda sukses selalu.
+        <br>
+        <br>
+        Kelompok 5
+        <br>
+        findDécor 💕
+    </p>
+</div>
+
+</div>
+</section>
+<!-- END TOKO -->
